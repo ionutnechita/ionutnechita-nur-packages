e2eb624159f15e92d841812cb47e8a9d861704b2
Ionut Nechita <ionut_n2001@yahoo.com>
2023-04-16 18:39:15 +0300

    NixNUR: linux_sunlight: init at 6.1.24
    Description:
     - linux_sunlight: init at 6.1.24
     - variant: lts
    
    Change-Id: Ifc1c999275e34d0e92de9fa78d709fa5272f8770
    Signed-off-by: Ionut Nechita <ionut_n2001@yahoo.com>

--- default.nix
+++ default.nix
@@ -15,6 +15,10 @@
   overlays = import ./overlays; # nixpkgs overlays
 
   example-package = pkgs.callPackage ./pkgs/example-package { };
-  # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
-  # ...
+  linux_sunlight = pkgs.callPackage ./pkgs/sunlight-kernels {
+    kernelPatches = [
+      pkgs.kernelPatches.bridge_stp_helper
+      pkgs.kernelPatches.request_key_helper
+    ];
+  };
 }
