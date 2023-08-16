ff8cdd216ce4c322e575473fd9205b0ffe1d1564
Ionut Nechita <ionut_n2001@yahoo.com>
2023-08-16 19:41:08 +0300

    NixNUR: linux_sunlight_mainline: 6.5.0-rc4 -> 6.5.0-rc6
    Description:
     - linux_sunlight: 6.5.0-rc6
     - variant: mainline
    
    Change-Id: I89e312e4ce7b2b0dc39b72b79f0c164e3ce8f874
    Signed-off-by: Ionut Nechita <ionut_n2001@yahoo.com>

--- pkgs/sunlight-kernels-mainline/default.nix
+++ pkgs/sunlight-kernels-mainline/default.nix
@@ -1,12 +1,12 @@
 { lib, stdenv, fetchFromGitHub, buildLinux, ... } @ args:
 
 let
-  modDirVersion = "6.5.0-rc4";
+  modDirVersion = "6.5.0-rc6";
   parts = lib.splitString "-" modDirVersion;
   version = lib.elemAt parts 0;
   suffix = lib.elemAt parts 1;
   extraVer = "";
-  hash = "sha256-QrnzFG6zDgesw+Q2pKZaB15Z1FzFcKHR51HJXYGPX3A=";
+  hash = "sha256-xQdqr388gHLc3fqBwozSyN5EDyyjFjc1ijmtRqaCm0M=";
 
   numbers = lib.splitString "." version;
   branch = "${lib.elemAt numbers 0}.${lib.elemAt numbers 1}";
@@ -18,7 +18,7 @@ buildLinux (args // rec {
     src = fetchFromGitHub {
       owner = "sunlightlinux";
       repo = "linux-sunlight";
-      rev = "5de078d6267b0f7746c9dc6febaaaf2f51d4f950";
+      rev = "15d47d0c2ab24b1eec29e5a762c799b2e76589b2";
       inherit hash;
     };
 
