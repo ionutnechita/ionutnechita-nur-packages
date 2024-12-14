726601aa274c1b85ecae724f3ee9c7244cddb83d
Ionut Nechita <ionut_n2001@yahoo.com>
2024-12-14 14:12:53 +0200

    NixNUR: linux_sunlight: 6.12.4
    Description:
     - linux_sunlight: 6.12.4
     - variant: lts
     - production environment
    
    Change-Id: I263826014ce321d788109fd8dc32b8a442253134
    Signed-off-by: Ionut Nechita <ionut_n2001@yahoo.com>

--- pkgs/sunlight-kernels/default.nix
+++ pkgs/sunlight-kernels/default.nix
@@ -1,7 +1,7 @@
 { lib, stdenv, fetchFromGitHub, buildLinux, ... } @ args:
 
 let
-  modDirVersion = "6.12.3-lowlatency-sunlight1";
+  modDirVersion = "6.12.4-lowlatency-sunlight1";
 
   parts = lib.splitString "-" modDirVersion;
 
@@ -14,7 +14,7 @@ let
 
   rev = "${version}-${flavour}-${suffix}";
 
-  hash = "sha256-c1+mOWS11Tddfy2KIomyDCy9j3or6SDJvhlS3w5+7FQ=";
+  hash = "sha256-Txzh0Rv4fdvQCcDcNd+R883yZhLD8JzO0JP2DgyaxvY=";
 in
 buildLinux (args // rec {
     inherit version modDirVersion;
